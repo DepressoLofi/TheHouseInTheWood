a6edd3937f4b0e762f51f4698d9d4ed6fcb3fde0
David Mg Mg <125610886+DepressoLofi@users.noreply.github.com>
2023-07-17 14:38:00 +0630

    add magic bullet prefab

---
+++ Assets/script/hallway run/magic.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Magic : MonoBehaviour
+{
+    public string emilyObjectName = "Emily";
+    [SerializeField] float magicSpeed = 4f;
+    private Vector2 direction;
+    private Rigidbody2D rb;
+    private Vector2 offset = new(0, 0.8f);
+
+
+    void Start()
+    {
+        GameObject emilyObject = GameObject.Find(emilyObjectName);
+        if (emilyObject != null)
+        {
+            direction = ((Vector2)emilyObject.transform.position - (Vector2)transform.position + offset).normalized;
+
+            rb = GetComponent<Rigidbody2D>();
+            rb.velocity = direction * magicSpeed;
+        }
+    }
+}
