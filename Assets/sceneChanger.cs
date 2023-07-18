b0d3e5ee39b9f6e0c481386dd8cdfff57eddf9ca
David Mg Mg <125610886+DepressoLofi@users.noreply.github.com>
2023-07-18 22:19:55 +0630

    merging hallway and main

---
+++ Assets/sceneChanger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneChange : MonoBehaviour
+{
+    public float changeTime;
+    public string sceneName;
+
+    private void Update()
+    {
+        changeTime = Time.deltaTime;
+        if (changeTime <= 0)
+        {
+            SceneManager.LoadScene(sceneName);
+
+        }
+
+    }
+}
